6a813fabdd694dd2c6f7bc0a9f48a9d7d2599d45
NirajK017 <nirajsk10912@gmail.com>
2025-01-10 21:45:31 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import LandingPage from './pages/LandingPage';
 import RoadmapPage from './pages/RoadmapPage';
 
