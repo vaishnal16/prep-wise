ef237800762760ce84e748bbcfae42a8c2000f6a
Vaishnal Mali <vaishnal16305@gmail.com>
2025-01-19 23:29:44 +0530

    updated backend

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,6 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0' ,() => {
   console.log(`Server running on port ${PORT}`);
 });
